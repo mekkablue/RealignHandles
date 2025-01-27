c1c3dd4e86e73d8cd3cafa88c3634aff6cf41bdf
mekkablue <rainer@schriftlabor.at>
2025-01-27 19:08:35 +0100

    Initial Commit

--- README.md
+++ README.md
@@ -1,2 +1,43 @@
 # RealignHandles
-Glyphs.app filter for fixing interpolation kinks
+
+This is a [Glyphs.app](http://glyphsapp.com/) filter for fixing interpolation kinks by realigning handles next to (green) curve nodes.
+
+![RealignHandles](RealignHandles.png)
+
+It is most useful as a custom parameter post interpolation. (Note that the parameter does not work in variable font exports because there is no post interpolation in VFs.)
+
+
+### Installation
+
+1. One-click install *Realign Handles* from *Window > Plugin Manager*
+2. Restart Glyphs.
+
+
+### Usage Instructions
+
+#### in Edit view
+
+1. Open a glyph for editing.
+2. Choose *Filter > Realign Handles.* Hold down Opt key for applying to all master layers at once.
+
+#### as custom parameter
+
+1. In *File > Font Info > Exports,* in one of the instances, add a *Filter* custom parameter.
+2. As its value, enter `RealignHandles`.
+3. If you want to limit it to certain glyphs only, add a semicolon, `include:` followed by comma-separated glyph name list, e.g., `RealignHandles; include: germandbls, dagger`
+
+The custom parameter only works for static instances.
+
+
+### License
+
+Copyright 2025 Rainer Erich Scheichelbauer (@mekkablue).
+Based on sample code by the Glyphs team (glyphsapp.com).
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+http://www.apache.org/licenses/LICENSE-2.0
+
+See the License file included in this repository for further details.
---
+++ RealignHandles.glyphsFilter/Contents/Resources/plugin.py
@@ -0,0 +1,121 @@
+# encoding: utf-8
+
+###########################################################################################################
+#
+#
+# Filter without dialog plug-in
+#
+# Read the docs:
+# https://github.com/schriftgestalt/GlyphsSDK/tree/master/Python%20Templates/Filter%20without%20Dialog
+#
+#
+###########################################################################################################
+
+from __future__ import division, print_function, unicode_literals
+import objc
+from GlyphsApp import Glyphs, GSSMOOTH, GSOFFCURVE
+from GlyphsApp.plugins import FilterWithoutDialog
+from AppKit import NSPoint, NSEvent, NSEventModifierFlagOption
+
+
+def triplet(n1, n2, n3):
+	return (*n1.position, *n2.position, *n3.position)
+
+
+def straightenBCPs(layer):
+
+	def closestPointOnLine(P, A, B):
+		# vector of line AB
+		AB = NSPoint(B.x - A.x, B.y - A.y)
+		# vector from point A to point P
+		AP = NSPoint(P.x - A.x, P.y - A.y)
+		# dot product of AB and AP
+		dotProduct = AB.x * AP.x + AB.y * AP.y
+		ABsquared = AB.x**2 + AB.y**2
+		t = dotProduct / ABsquared
+		x = A.x + t * AB.x
+		y = A.y + t * AB.y
+		return NSPoint(x, y)
+
+	def ortho(n1, n2):
+		xDiff = n1.x - n2.x
+		yDiff = n1.y - n2.y
+		# must not have the same coordinates,
+		# and either vertical or horizontal:
+		if xDiff != yDiff and xDiff * yDiff == 0.0:
+			return True
+		return False
+
+	handleCount = 0
+	for p in layer.paths:
+		for n in p.nodes:
+			if n.connection != GSSMOOTH:
+				continue
+			nn, pn = n.nextNode, n.prevNode
+			if all((nn.type == GSOFFCURVE, pn.type == GSOFFCURVE)):
+				# surrounding points are BCPs
+				smoothen, center, opposite = None, None, None
+				for handle in (nn, pn):
+					if ortho(handle, n):
+						center = n
+						opposite = handle
+						smoothen = nn if nn != handle else pn
+						oldPos = triplet(smoothen, center, opposite)
+						p.setSmooth_withCenterNode_oppositeNode_(smoothen, center, opposite)
+						if oldPos != triplet(smoothen, center, opposite):
+							handleCount += 1
+						break
+				if smoothen == center == opposite is None:
+					oldPos = triplet(n, nn, pn)
+					n.position = closestPointOnLine(n.position, nn, pn)
+					if oldPos != triplet(n, nn, pn):
+						handleCount += 1
+			elif n.type != GSOFFCURVE and (nn.type, pn.type).count(GSOFFCURVE) == 1:
+				# only one of the surrounding points is a BCP
+				center = n
+				if nn.type == GSOFFCURVE:
+					smoothen = nn
+					opposite = pn
+				elif pn.type == GSOFFCURVE:
+					smoothen = pn
+					opposite = nn
+				else:
+					continue  # should never occur
+				oldPos = triplet(smoothen, center, opposite)
+				p.setSmooth_withCenterNode_oppositeNode_(smoothen, center, opposite)
+				if oldPos != triplet(smoothen, center, opposite):
+					handleCount += 1
+	return handleCount
+
+
+class RealignHandles(FilterWithoutDialog):
+
+	@objc.python_method
+	def settings(self):
+		self.menuName = Glyphs.localize({
+			'en': 'Realign Handles',
+			'de': 'Anfasser neu ausrichten',
+			'fr': 'Realigner poignées',
+			'es': 'Realinear manejadores',
+		})
+
+	@objc.python_method
+	def filter(self, layer, inEditView, customParameters):
+		shouldProcessAllMasters = False
+		if inEditView:
+			keysPressed = NSEvent.modifierFlags()
+			shouldProcessAllMasters = keysPressed & NSEventModifierFlagOption == NSEventModifierFlagOption
+		
+		glyph = layer.parent
+		if shouldProcessAllMasters:
+			for everyLayer in glyph.layers:
+				if everyLayer.isMasterLayer or everyLayer.isSpecialLayer:
+					straightenBCPs(everyLayer)
+		else:
+			straightenBCPs(layer)
+
+
+	@objc.python_method
+	def __file__(self):
+		"""Please leave this method unchanged"""
+		return __file__
